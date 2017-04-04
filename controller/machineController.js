0aecd12e033a1088d13d9da59ad017248433b57d
naveed <naveed.ajaj@gmail.com>
2017-04-04 23:43:40 +0530

    versioned now

---
+++ controller/machineController.js
@@ -0,0 +1,20 @@
+angular.module('loggerApp', [])
+.controller('MachineController', function($scope) {
+appController=$scope;
+$scope.setOperators=function (optgridData){
+  $scope.operators=optgridData;
+  $scope.$apply();
+};
+$scope.setJobs=function (jobsData){
+  $scope.jobs=jobsData;
+  $scope.$apply();
+};
+$scope.jobChange=function (macid,seljob){
+
+  var  url = "http://pi.trendzsoft.in/api/updatemachinestatus.php?" + "type='op'&tval='"+ seljob.id +"'&ip='" + macid + "'";
+  $.getJSON(url, function( sdata ) {
+
+  $('#loader1').hide();
+  });
+}
+});
